9837f69c21a9e915cb2ed01743d8a6a6a553cc40
LittleCompiler <febbyndaila@gmail.com>
2026-02-22 17:41:36 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home Maintenance</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h2><a href="pages/auth/register.html">Register</a></h2>
+   
+
+<script src="script.js"></script>
+</body>
+</html>
